6bf8b3daac914f880eb697a5a2b03f2e7fd6592a
Suchitra Sompalli <suchitra.sompalli@gmail.com>
2018-05-06 20:40:13 -0500

    feat: added action bar with title and overflow menu

--- app/src/main/java/com/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/popularmovies/MainActivity.java
@@ -2,12 +2,39 @@ package com.udacity.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.most_popular:
+            Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG).show();
+            return(true);
+        case R.id.top_rated:
+            Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG).show();
+            return(true);
+
+    }
+        return(super.onOptionsItemSelected(item));
+    }
+
 }
