4cc14e9d399295413f47480de729cf60598a2610
Suchitra Sompalli <suchitra.sompalli@gmail.com>
2018-05-24 08:55:11 -0500

    feat: use safer opt for json key

--- app/src/main/java/com/udacity/popularmovies/utils/MovieJsonUtils.java
+++ app/src/main/java/com/udacity/popularmovies/utils/MovieJsonUtils.java
@@ -1,7 +1,7 @@
 package com.udacity.popularmovies.utils;
 
 /*
-  Created by suchitrasompalli on 5/11/2018.
+  Created by suchitrasompalli on 5/20/2018.
  */
 
 import android.content.Context;
@@ -20,6 +20,14 @@ import java.net.HttpURLConnection;
  */
 public final class MovieJsonUtils {
 
+    private static final String JSON_MOVIE_RESULTS_KEY = "results";
+    private static final String JSON_ID_KEY = "id";
+    private static final String JSON_TITLE_KEY = "results";
+    private static final String JSON_RELEASE_DATE_KEY = "release_date";
+    private static final String JSON_POSTER_PATH_KEY = "poster_path";
+    private static final String JSON_OVERVIEW_KEY = "overview";
+    private static final String JSON_VOTE_KEY = "vote_average";
+
     /**
      * This method parses JSON from a web response and returns an array of movie poster url
      * representing movie poster.
@@ -33,25 +41,23 @@ public final class MovieJsonUtils {
     public static Movie[] getMoviesFromJson(Context context, String movieJsonStr)
             throws JSONException {
 
-        final String MOVIE_RESULTS = "results";
-
         /* String array to hold movie poster String */
         Movie[] parsedMovieData = null;
 
         JSONObject movieJson = new JSONObject(movieJsonStr);
-        JSONArray resultsArray = movieJson.getJSONArray(MOVIE_RESULTS);
+        JSONArray resultsArray = movieJson.optJSONArray(JSON_MOVIE_RESULTS_KEY);
 
         parsedMovieData = new Movie[resultsArray.length()];
 
         for (int i = 0; i < resultsArray.length(); i++) {
              /* Get the JSON object representing a movie */
-            JSONObject movie_json = resultsArray.getJSONObject(i);
-            int id = Integer.parseInt(movie_json.getString("id"));
-            String title = movie_json.getString("title");
-            String release_date = movie_json.getString("release_date");
-            String poster_path = movie_json.getString("poster_path");
-            String overview = movie_json.getString("overview");
-            String vote_average = movie_json.getString("vote_average");
+            JSONObject movie_json = resultsArray.optJSONObject(i);
+            int id = Integer.parseInt(movie_json.optString(JSON_ID_KEY));
+            String title = movie_json.optString(JSON_TITLE_KEY);
+            String release_date = movie_json.optString(JSON_RELEASE_DATE_KEY);
+            String poster_path = movie_json.optString(JSON_POSTER_PATH_KEY);
+            String overview = movie_json.optString(JSON_OVERVIEW_KEY);
+            String vote_average = movie_json.optString(JSON_VOTE_KEY);
 
             parsedMovieData[i] = new Movie(id, title, poster_path, vote_average, release_date,
                     overview);
